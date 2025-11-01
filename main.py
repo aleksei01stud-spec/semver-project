40d6f0dfba4453cb5396c3dd034deaf52694415e
Aleksei <aleksei01stud@gmail.com>
2025-11-01 08:24:22 +0100

    v1.2.0
    //добавили, чтобы программа считала буквы имени

--- main.py
+++ main.py
@@ -5,3 +5,5 @@ print(f"Nice to meet you, {name}!")
 if name == "":  
     name = "friend"
 print(f"Nice to meet you, {name}!")
+print(f"Nice to meet you, {name}!")
+print(f"Your name has {len(name)} letters!")
